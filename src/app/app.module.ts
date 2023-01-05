c6db52de8c2e4e02a022755c6c5b7de274330c55
Alexandre Vermeersch <112808930+AlexVerm12@users.noreply.github.com>
2023-01-05 19:44:06 +0100

    added hero and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    MenuComponent,
+    HeroComponent,
+    AboutMeComponent,
+    ContactComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
